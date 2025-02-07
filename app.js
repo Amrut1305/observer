083250ed38d17aebad2c45b2bca53cb5657a4130
Amrut13 <amrut.shete13@gmail.com>
2025-02-07 07:57:56 +0530

    changes done

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 let cards = document.querySelectorAll(".card")
 
-let option ={
-    root:null,
-    threshold:0,
-    rootMargin: "-150px"
-}
-let observer = new IntersectionObserver(callback=(entries,observer)=>{
-    entries.forEach(entry=>{
-        if(!entry.isIntersecting){
-            return
-        }
-        console.log(entry.target);  
-        entry.target.classList.toggle('animate__animated')
-        entry.target.classList.toggle('animate__backInRight')
-        entry.target.classList.toggle('animate__repeat-1')
-        entry.target.classList.toggle('animate_slower')
-        entry.target.classList.toggle('3s')
-    })
-},option)
+document.addEventListener('DOMContentLoaded', ()=>{
+    let option ={
+        root:null,
+        threshold:0,
+        rootMargin: "-150px"
+    }
+    let observer = new IntersectionObserver(callback=(entries,observer)=>{
+        entries.forEach(entry=>{
+            if(!entry.isIntersecting){
+                return
+            }
+            entry.target.classList.toggle('animate__animated')
+            entry.target.classList.toggle('animate__heartBeat')
+            entry.target.classList.toggle('animate__repeat-1')
+            entry.target.classList.toggle('animate_slower')
+            entry.target.classList.toggle('3s')
+            
+        })
+    },option)
 
-cards.forEach(card=>observer.observe(card))
+    cards.forEach(card=>observer.observe(card))
+})
 
